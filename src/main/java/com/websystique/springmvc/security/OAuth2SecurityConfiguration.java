de324205c6d24ce452c152d76358eb1cb622e5e9
poriaux <michel.poriaux@condorcet.be>
2018-08-21 17:51:52 +0200

    commit6

--- src/main/java/com/websystique/springmvc/security/OAuth2SecurityConfiguration.java
+++ src/main/java/com/websystique/springmvc/security/OAuth2SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
         
-        System.out.println("====>authentification");
+        System.out.println("======>authentification");
         auth.inMemoryAuthentication()
                 .withUser("bill").password("abc123").roles("ADMIN").and()
                 .withUser("bob").password("abc123").roles("USER");
